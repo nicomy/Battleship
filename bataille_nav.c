4bc349652ee77259284a39cf15582a2b28f2bfd1
Nicomy <nicomy_68@hotmail.fr>
2016-03-23 15:05:17 +0100

    mise en ligne du fichier

---
+++ bataille_nav.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+
+
+typedef char** grille ; 
+
+void init_grille( grille g ){
+
+}
+
+void afficher_jeu(grille g) {
+	int i, j ; 
+
+	for(i=0, )
+}
+
+
+int main(int argc, char* argv[]){
+	//argc = taille du tableau et argv est un tableau qui conntient les argument en string. 
+    // si argc = 1 alors le tableau continet qu'un seul élement argv[0] = nom du prog
+
+
+
+}
