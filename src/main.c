d8342d44c8d93c012d606866889f5ee04fc74d04
nicomy117 <nicomy_68@hotmail.fr>
2016-04-09 13:27:06 +0200

    main.c fonctionnel pb pour mise à jour des bateaux qui coulé.

--- src/main.c
+++ src/main.c
@@ -7,20 +7,31 @@
 #include "codageNavires.h"
 
 
+int char_to_int(const char* t){
+	int res= 0 , i =0; 
+	while(t[i]!='\0'){
+		res = res*10 + t[i] -'0';
+		i++;
+	}
+	return res;
+}
+
+
 int main(int argc, char const *argv[])
 {
-	N = argv[1] - '0' ;
+	int N = char_to_int(argv[1]);//argv[1][0] - '0' ;
 	int compteur = 0,i ,j ;
 	char coup[10] ; 
 	grille gc = init_grille ( N , ' ' ) , g = init_grille(N, 'B') ;
 	liste_navire l ;
 
 	printf("Le joueur 1 doit placer les bateaux \n");
-	usr_remplir_grille(g, N );
+	//usr_remplir_grille(g, N );
+	remplir_gille(g,N) ;
 	l = cree_liste_navires(g, N);
 
 	printf("au joueur 2 d'essayer de couler les navires.\n");
-	while(jeu_fini(l)){
+	while(!jeu_fini(l)){
 		printf("Voici la grille\n");
 		affiche_etat_coules(gc, N );
 		printf("veuillez entrer les coordonées du tir (ex: \"E5\")\n");
@@ -30,10 +41,10 @@ int main(int argc, char const *argv[])
 			printf("coordonnée trop court il faut rentrer les 2 paramètre colonne et ligne\n");
 		}
 		else{
-			i = coup[0] - 'A' ;
-			j= coup[1] -'0' ;
+			j = coup[0] - 'A' ;
+			i = coup[1] -'0' ;
 
-			if (i<0 || j<0 || i>(n-1) || j > n-1)
+			if (i<0 || j<0 || i>(N-1) || j > N-1)
 			{
 				printf("\n\nVos coordonnées sortent du tableau ou son invalide \n\n");
 			}
