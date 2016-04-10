628c77e5ed47dc04e31c8b766c1a6d81ecb87433
AntoineBL <antoineblanc269@laposte.net>
2016-04-10 17:55:16 +0200

    ajout de integer

---
+++ Version 1/include/integer.h
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct $maillon* pMaillon;
+typedef struct $maillon
+{
+	int i_deb;
+	int j_deb;
+	int i_fin;
+	int j_fin;
+	int coule;
+	pMaillon nextMaillon;
+}maillon;
+
+
+
+int get_i_deb(pMaillon m);
+int get_i_fin(pMaillon m);
+void set_i_deb(pMaillon m, int i);
+void set_i_fin(pMaillon m, int i);
+int get_j_deb(pMaillon m);
+int get_j_fin(pMaillon m);
+void set_j_deb(pMaillon m, int j);
+void set_j_fin(pMaillon m, int j);
+void set_coule(pMaillon m, int c);
+int get_coule(pMaillon m);
+
+/*
+int get_field(uint32_t a, int faible, int fort);
+
+int set_field(uint32_t a, int faible, int fort , int v);*/
---
+++ Version 1/src/integer.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "integer.h"
+
+
+
+
+int get_i_deb(pMaillon m){
+	return m->i_deb;
+}
+
+int get_i_fin(pMaillon m){
+	return m->i_fin;
+}
+
+void set_i_deb(pMaillon m, int i){
+	m->i_deb = i;
+}
+
+void set_i_fin(pMaillon m, int i){
+ 	m->i_fin = i;
+}
+
+int get_j_deb(pMaillon m){
+	return m->j_deb;
+}
+
+int get_j_fin(pMaillon m){
+	return m->j_fin;
+}
+
+void set_j_deb(pMaillon m, int j){
+	m->j_deb = j;
+}
+
+void set_j_fin(pMaillon m, int j){
+	m->j_fin = j;
+}
+
+void set_coule(pMaillon m, int c){
+	m->coule = c;
+}
+
+int get_coule(pMaillon m){
+	return m->coule;
+}
+
+
