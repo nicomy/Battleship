be4ec2a8f4600793e0eb359775fad36987c5e201
nicomy117 <nicomy_68@hotmail.fr>
2016-04-10 20:58:44 +0200

    début de commentaire de binaire.c

--- Version 2/src/binaire.c
+++ Version 2/src/binaire.c
@@ -8,23 +8,22 @@ int get_field(uint32_t a, int faible, int fort){
 	uint32_t tmp = a ;
 	int bordg= 31 - fort ;
 
-	tmp = tmp << bordg ;
-	tmp = tmp >> (bordg + faible-1) ;
+	tmp = tmp << bordg ; //on enlève tous les bits à gauche du bit fort
+	tmp = tmp >> (bordg + faible-1) ; //puis on décalle jusqu'à que le bit faible soit au niveau du bit 0 afin qu'on puisse directement lire le bit.
 
 	return tmp ; 
 }
 
 uint32_t set_field(uint32_t a, int faible, int fort , int v){
-	uint32_t masque = ~0 ;
+	uint32_t masque = ~0 ; // met masque à que des 1 
 	int bordg = 31 - fort ; 
 
-	masque = masque << bordg ; 
-	masque = masque >> (bordg + faible-1) ;
-	masque = masque <<(faible -1);
-	masque = ~masque ;
-	v = v << (faible -1); 
-	a = a & masque ; 
-	a = a + v ; 
+	masque = masque << bordg ; //on enlève tous 1 à gauche du bit fort
+	masque = masque >> (bordg + faible-1) ;// enlève tous les 1 à droite de faible 
+	masque = masque <<(faible -1); // replace le masque au debuté du bit faible
+	v = v << (faible -1);  // on décale pareillement v pour être alligné avec les bon bit
+	a = a & masque ; //on met à 0 tout les bits dans l'intervalle failbe-fort 
+	a = a + v ; // on place les bit de v.
 	//printf("%d\n", a);
 	return a ; 
 }
