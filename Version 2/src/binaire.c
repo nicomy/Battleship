eca6414bc4698be8c615d4e8d056c51bcec5936b
AntoineBL <antoineblanc269@laposte.net>
2016-04-10 17:00:14 +0200

    commentaire en moins

--- Version 2/src/binaire.c
+++ Version 2/src/binaire.c
@@ -73,22 +73,3 @@ int get_coule(pMaillon m){
 	return get_field(m->c,31, 31);
 }
 
-/*
-int main(int argc, char const *argv[])
-{
-	uint32_t a,b = 256 + 2048  +15 + 40100;
-	//b  ;
-	printf("%d\n",b );
-	printf("%d\n", get_field(b,8,11) ); 
-	printf("%d\n", b );
-	a = set_field(b,8,10,11);
-	printf("%d\n", a );
-	printf("%d\n", get_field(a,8,11) );
-	
-
-
-
-
-	return 0;
-}
-*/
